6bb45799d38b4c7c411e3f8c4cd9a3a3c5f02214
milank <milankornicer@gmail.com>
2023-05-06 11:27:51 -0400

    feat: husky, eslint, prettier,

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 module.exports = {
   reactStrictMode: true,
+  eslint: {
+    // TODO: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   images: {
     domains: [
       "zvzegsqprutvkaafnfdh.supabase.in",
