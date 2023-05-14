f825288cad691fa2643142325c22736851b7b740
milank <milankornicer@gmail.com>
2023-05-13 23:11:40 -0400

    tsconfig for cypress

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,8 +2,9 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   e2e: {
+    baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // node event listeners here
     },
   },
 })
