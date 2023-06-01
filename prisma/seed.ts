e48262a754d786596c222a09970a04820f571b7b
milank <milankornicer@gmail.com>
2023-05-31 20:34:35 -0400

    seed file update

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,25 +1,47 @@
 import { PrismaClient } from '@prisma/client'
+
 const prisma = new PrismaClient()
 
-async function main() {
-  const id = 'cl9ebqhxk00003b600tymydho'
-  await prisma.user.upsert({
-    where: {
-      id,
-    },
-    create: {
-      id,
-    },
-    update: {},
-  })
+async function seed() {
+  // Create 40 users
+  const users = []
+  for (let i = 0; i < 40; i++) {
+    const user = await prisma.user.create({
+      data: {
+        name: `User ${i + 1}`,
+        email: `user${i + 1}@example.com`,
+        nickname: `user${i + 1}`,
+      },
+    })
+    users.push(user)
+  }
+
+  // Create 40 stamps with associated users
+  for (let i = 0; i < 40; i++) {
+    await prisma.stamp.create({
+      data: {
+        userId: users[i].id,
+        game: `Game ${i + 1}`,
+        title: `Stamp ${i + 1}`,
+        description: `Description for Stamp ${i + 1}`,
+        category: i % 3 ? 'Housing' : 'Production Chain',
+        region: i % 2 ? 'Old World' : 'New World',
+        screenshot: '/stamp-highlight.jpg',
+        stamp: 'stampFile',
+        modded: i % 2 === 0,
+        liked: i,
+        downloads: i,
+      },
+    })
+  }
+
+  console.log('Seed completed successfully')
 }
 
-main()
-  .then(async () => {
-    await prisma.$disconnect()
+seed()
+  .catch((error) => {
+    console.error(error)
   })
-  .catch(async (e) => {
-    console.error(e)
+  .finally(async () => {
     await prisma.$disconnect()
-    process.exit(1)
   })
