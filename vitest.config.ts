18ad82c2ae9f41d82d14b0cd9f2b9e501d2622cc
mkcy3 <milankornicer@gmail.com>
2023-08-27 22:16:33 -0400

    use esnext in target tsconfig, remove js

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,10 +1,11 @@
 /// <reference types="vitest" />
 
 import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [tsconfigPaths(), react()],
   test: {
     environment: 'jsdom',
     globals: true,
