c737ca79503d84f0e633337b6981ba6ce761974a
mkcy3 <milankornicer@gmail.com>
2023-09-05 13:01:39 -0400

    remove google-analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,6 @@
 import '../styles/globals.css'
 
 import type { AppProps } from 'next/app'
-import Script from 'next/script'
 import { SessionProvider } from 'next-auth/react'
 import { Toaster } from 'react-hot-toast'
 
@@ -11,30 +10,12 @@ const StampsApp = ({
   pageProps: { session, ...pageProps },
 }: AppProps) => {
   return (
-    <>
-      <Script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=G-9KT01SRSVX"
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          
-          window.dataLayer = window.dataLayer || [];
-          
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-9KT01SRSVX');
-        `}
-      </Script>
-      <SessionProvider session={session}>
-        <Layout>
-          <Component {...pageProps} className="font-sans" />
-          <Toaster />
-        </Layout>
-      </SessionProvider>
-    </>
+    <SessionProvider session={session}>
+      <Layout>
+        <Component {...pageProps} className="font-sans" />
+        <Toaster />
+      </Layout>
+    </SessionProvider>
   )
 }
 
