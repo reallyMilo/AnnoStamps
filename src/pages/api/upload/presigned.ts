a1cdbf714b384534d3558d724bb1e99db8a8798d
Milan K <57148574+mkcy3@users.noreply.github.com>
2023-10-04 11:14:00 -0400

    get presigned url (#126)

---
+++ src/pages/api/upload/presigned.ts
@@ -0,0 +1,32 @@
+import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3'
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
+import { NextApiRequest, NextApiResponse } from 'next'
+import { getServerSession } from 'next-auth'
+
+import { authOptions } from '../auth/[...nextauth]'
+
+interface Req extends NextApiRequest {
+  query: { fileName: string; fileType: string }
+}
+export default async function presignedHandler(req: Req, res: NextApiResponse) {
+  const session = await getServerSession(req, res, authOptions)
+
+  if (!session || !session?.user?.id) {
+    return res.status(401).json({ ok: false, message: 'Unauthorized.' })
+  }
+
+  const { AWS_S3_REGION, AWS_S3_BUCKET } = process.env
+
+  const { fileName, fileType } = req.query
+
+  const client = new S3Client({ region: AWS_S3_REGION })
+  const command = new PutObjectCommand({
+    Bucket: AWS_S3_BUCKET,
+    Key: fileName,
+    ContentType: fileType,
+  })
+  //@ts-expect-error client type error
+  const url = await getSignedUrl(client, command, { expiresIn: 3600 })
+
+  return res.status(200).json({ url })
+}
