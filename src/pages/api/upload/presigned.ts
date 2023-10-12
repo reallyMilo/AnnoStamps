d57b05f19eedcf26031a6df4c50407b99c81a0d5
milank <milankornicer@gmail.com>
2023-10-12 17:32:51 -0400

    upload file to S3

--- src/pages/api/upload/presigned.ts
+++ src/pages/api/upload/presigned.ts
@@ -1,32 +1,39 @@
 import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3'
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
+import { nanoid } from 'nanoid'
 import { NextApiRequest, NextApiResponse } from 'next'
 import { getServerSession } from 'next-auth'
 
 import { authOptions } from '../auth/[...nextauth]'
 
 interface Req extends NextApiRequest {
-  query: { fileName: string; fileType: string }
+  query: { fileType: string; filename: string }
 }
 export default async function presignedHandler(req: Req, res: NextApiResponse) {
   const session = await getServerSession(req, res, authOptions)
 
-  if (!session || !session?.user?.id) {
+  if (!session?.user?.id) {
     return res.status(401).json({ ok: false, message: 'Unauthorized.' })
   }
 
-  const { AWS_S3_REGION, AWS_S3_BUCKET } = process.env
+  const { AWS_S3_REGION, AWS_S3_BUCKET, AWS_S3_BUCKET_FOLDER } = process.env
 
-  const { fileName, fileType } = req.query
+  const { filename, fileType } = req.query
 
+  const type = fileType === 'zip' ? 'stamps' : 'images'
+
+  const path = `${AWS_S3_BUCKET_FOLDER}/${session.user.id}/${type}/${nanoid(
+    16
+  )}`
   const client = new S3Client({ region: AWS_S3_REGION })
   const command = new PutObjectCommand({
     Bucket: AWS_S3_BUCKET,
-    Key: fileName,
+    Key: path,
     ContentType: fileType,
+    Metadata: { filename },
   })
 
   const url = await getSignedUrl(client, command, { expiresIn: 3600 })
 
-  return res.status(200).json({ url })
+  return res.status(200).json({ url, path })
 }
