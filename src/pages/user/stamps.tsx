a95c9b7d325f6eb350abfdd139e0f6e021f53437
milank <milankornicer@gmail.com>
2023-12-21 17:47:52 -0500

    feat: users able to delete stamps

--- src/pages/user/stamps.tsx
+++ src/pages/user/stamps.tsx
@@ -1,10 +1,101 @@
-import { PencilSquareIcon } from '@heroicons/react/20/solid'
+import { Dialog, Transition } from '@headlessui/react'
+import { PencilSquareIcon, TrashIcon } from '@heroicons/react/20/solid'
 import Link from 'next/link'
+import { useRouter } from 'next/router'
+import { Fragment, useState } from 'react'
 
 import Grid from '@/components/Layout/Grid'
 import StampCard from '@/components/StampCard'
 import Container from '@/components/ui/Container'
 import { useUserStamps } from '@/lib/hooks/useUserStamps'
+
+const StampDeleteModal = ({
+  id,
+  title,
+}: NonNullable<
+  Pick<ReturnType<typeof useUserStamps>, 'userStamps'>['userStamps']
+>['listedStamps'][0]) => {
+  const [isOpen, setIsOpen] = useState(false)
+  const router = useRouter()
+
+  const deleteStamp = async () => {
+    const deleteStampRes = await fetch(`/api/stamp/${id}`, {
+      method: 'DELETE',
+    })
+
+    if (!deleteStampRes.ok) {
+      return
+    }
+    router.reload()
+  }
+  return (
+    <>
+      <button className="rounded-md px-4 py-2" onClick={() => setIsOpen(true)}>
+        <TrashIcon className="h-5 w-5 text-red-600" />
+      </button>
+
+      <Transition appear show={isOpen} as={Fragment}>
+        <Dialog
+          as="div"
+          className="relative z-10"
+          onClose={() => setIsOpen(false)}
+        >
+          <Transition.Child
+            as={Fragment}
+            enter="ease-out duration-300"
+            enterFrom="opacity-0"
+            enterTo="opacity-100"
+            leave="ease-in duration-200"
+            leaveFrom="opacity-100"
+            leaveTo="opacity-0"
+          >
+            <div className="fixed inset-0 bg-black/25" />
+          </Transition.Child>
+
+          <div className="fixed inset-0 overflow-y-auto">
+            <div className="flex min-h-full items-center justify-center p-4 text-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="w-full max-w-md transform space-y-5 overflow-hidden rounded-2xl bg-gray-200 p-6 text-left align-middle shadow-xl transition-all">
+                  <Dialog.Title
+                    as="h3"
+                    className="text-lg font-medium leading-6 text-gray-900"
+                  >
+                    Delete {title}?
+                  </Dialog.Title>
+
+                  <p>This action cannot be undone.</p>
+                  <div className="flex justify-between">
+                    <button
+                      className="rounded-md border border-black px-4 py-2 hover:bg-gray-100"
+                      onClick={() => setIsOpen(false)}
+                    >
+                      No, cancel
+                    </button>
+
+                    <button
+                      className="rounded-md bg-red-600 px-4 py-2 font-bold text-white hover:bg-red-400"
+                      onClick={deleteStamp}
+                    >
+                      Yes, delete
+                    </button>
+                  </div>
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </div>
+        </Dialog>
+      </Transition>
+    </>
+  )
+}
 
 const Stamps = () => {
   const { isLoading, error, userStamps } = useUserStamps()
@@ -33,15 +124,19 @@ const Stamps = () => {
         ) : (
           userStamps?.listedStamps.map((stamp) => (
             <div key={stamp.id} className="flex flex-col">
-              <Link
-                className="mb-1 ml-auto flex rounded-md bg-[#6DD3C0] px-4 py-2 text-sm font-bold text-[#222939] transition hover:bg-rose-500 focus:outline-none focus:ring-4 focus:ring-rose-500 focus:ring-opacity-50"
-                href={{
-                  pathname: `/user/[stamp]`,
-                  query: { stamp: stamp.id },
-                }}
-              >
-                <PencilSquareIcon className="mr-2 h-5 w-5" /> Edit Stamp{' '}
-              </Link>
+              <div className="mb-1 flex">
+                <StampDeleteModal {...stamp} />
+
+                <Link
+                  className="mb-1 ml-auto flex rounded-md bg-[#6DD3C0] px-4 py-2 text-sm font-bold text-[#222939] transition hover:bg-rose-500 focus:outline-none focus:ring-4 focus:ring-rose-500 focus:ring-opacity-50"
+                  href={{
+                    pathname: `/user/[stamp]`,
+                    query: { stamp: stamp.id },
+                  }}
+                >
+                  <PencilSquareIcon className="mr-2 h-5 w-5" /> Edit Stamp{' '}
+                </Link>
+              </div>
 
               <StampCard user={userStamps} {...stamp} />
             </div>
