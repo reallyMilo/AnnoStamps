ce35c186d43f92678f9c70b05ca1b39d0bf11907
milank <milankornicer@gmail.com>
2024-01-04 14:24:47 -0500

    test: update username_page

--- cypress/e2e/username_page.cy.ts
+++ cypress/e2e/username_page.cy.ts
@@ -1,15 +1,17 @@
 describe('Username Page', () => {
   it('displays user stamps', () => {
     cy.visit('/user1')
-    cy.intercept('get', '/api/user/user1', {
+    cy.intercept('get', '/api/user', {
       fixture: 'userStamps',
     })
 
     cy.get('#stamp-title').should('be.visible')
   })
 
-  it('displays error message for user not found', () => {
-    cy.visit('/no-user')
-    cy.contains('User not found')
+  it('displays 404 page', () => {
+    cy.request({ url: '/no-user', failOnStatusCode: false })
+      .its('status')
+      .should('equal', 404)
+    cy.visit('/no-user', { failOnStatusCode: false })
   })
 })
