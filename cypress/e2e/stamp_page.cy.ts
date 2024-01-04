6930fa50ab7df44555673daefa938ae5f3f98f35
Milan K <57148574+mkcy3@users.noreply.github.com>
2024-01-04 14:24:04 -0500

    feat: download stamp file as title (#175)

--- cypress/e2e/stamp_page.cy.ts
+++ cypress/e2e/stamp_page.cy.ts
@@ -1,3 +1,5 @@
+const path = require('path') // eslint-disable-line
+
 describe('Stamp Page', () => {
   beforeEach(() => {
     cy.visit('/')
@@ -9,7 +11,9 @@ describe('Stamp Page', () => {
   })
 
   it('download button should download stamp', () => {
-    cy.intercept('GET', '/stamp.zip').as('download')
+    cy.intercept('GET', '/stamp.zip', {
+      fixture: 'test-stamp.zip',
+    }).as('download')
     cy.getBySel('stamp-download')
       .trigger('mouseover')
       .then(($link) => {
@@ -20,9 +24,11 @@ describe('Stamp Page', () => {
     cy.wait('@download').then((req) => {
       expect(req.response.statusCode).to.equal(200)
     })
-    // const downloadsFolder = Cypress.config('downloadsFolder')
-    // cy.get('h1').then((h1) => {
-    //   cy.readFile(downloadsFolder + '/' + h1.text())
-    // })
+    const downloadsFolder = Cypress.config('downloadsFolder')
+
+    cy.get('h1').then((h1) => {
+      const filename = path.join(downloadsFolder, `${h1.text().trimEnd()}.zip`)
+      cy.readFile(filename, { timeout: 15000 }).should('have.length.gt', 50)
+    })
   })
 })
--- src/pages/stamp/[id].tsx
+++ src/pages/stamp/[id].tsx
@@ -152,6 +152,7 @@ const StampPage = ({ stamp }: { stamp: StampWithRelations }) => {
           data-testid="stamp-download"
           className="inline-block rounded-md bg-[#6DD3C0] px-4 py-2 font-bold"
           onClick={() => fetch(`/api/stamp/download/${id}`)}
+          download={title}
         >
           <ArrowDownTrayIcon className="mr-2 inline-block h-6 w-6" />
           Download
