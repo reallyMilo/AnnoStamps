b20e55d3ec6ab0ec53cff439d1bf82e376785e4c
mkcy3 <milankornicer@gmail.com>
2024-01-06 12:08:58 -0500

    fix: vitest plugin type error

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,10 +2,11 @@
 
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
-import { defineConfig } from 'vitest/config'
+
+import { UserConfig, defineConfig } from 'vitest/config'
 
 export default defineConfig({
-  plugins: [tsconfigPaths(), react()],
+  plugins: [tsconfigPaths(), react()] as UserConfig['plugins'],
   test: {
     environment: 'jsdom',
     globals: true,
