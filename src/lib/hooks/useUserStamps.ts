f805684d9851ab15cb1fafaa696c2829cc66308b
milank <milankornicer@gmail.com>
2024-01-23 18:09:23 -0500

    chore: auth middleware for protected routes

--- src/lib/hooks/useUserStamps.ts
+++ src/lib/hooks/useUserStamps.ts
@@ -1,25 +1,16 @@
-import { useSession } from 'next-auth/react'
 import useSWR from 'swr'
 
-import { displayAuthModal, fetcher } from '@/lib/utils'
+import { fetcher } from '@/lib/utils'
 
 import { UserWithStamps } from '../prisma/queries'
 
 export const useUserStamps = () => {
-  const { status } = useSession({
-    required: true,
-    onUnauthenticated() {
-      displayAuthModal()
-    },
-  })
-
   const { data, isLoading, error } = useSWR<{ data: UserWithStamps }>(
-    status === 'authenticated' ? '/api/user' : null,
+    '/api/user',
     fetcher
   )
 
   return {
-    status,
     userStamps: data?.data,
     isLoading,
     error,
