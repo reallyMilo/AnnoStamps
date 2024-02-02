4acfda3fe267b66f99cdcfdc04876e5108859a42
milank <milankornicer@gmail.com>
2024-02-02 10:10:19 -0500

    combine navbar and footer

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,7 +1,114 @@
 import Head from 'next/head'
+import Image from 'next/image'
+import Link from 'next/link'
 
-import Footer from './Footer'
-import Navbar from './Navbar'
+import Search from '../Filter/Search'
+
+const navigation = [
+  {
+    href: '/',
+    text: 'All Stamps',
+  },
+  {
+    href: '/how-to',
+    text: 'How To',
+  },
+]
+const socials = [
+  {
+    name: 'discord',
+    url: 'https://discord.gg/73hfP54qXe',
+    src: '/discord-white-icon.svg',
+  },
+  {
+    name: 'github',
+    url: 'https://github.com/reallyMilo/AnnoStamps',
+    src: '/github-mark.svg',
+  },
+]
+
+const Navbar = () => {
+  return (
+    <header className="relative z-20 h-20 w-full bg-[#222939] shadow-md">
+      <div className="container mx-auto h-full">
+        <div className="flex h-full items-center justify-between space-x-4 px-1 md:px-4">
+          <div className="flex md:space-x-6">
+            <Link
+              id="header-logo"
+              href="/"
+              className="hidden items-center space-x-1 md:flex"
+            >
+              <Image
+                src="/anno-stamps-logo.svg"
+                width="160"
+                height="60"
+                alt="Anno Stamps"
+                priority
+                style={{
+                  width: '100%',
+                  maxWidth: '160px',
+                  height: 'auto',
+                }}
+                className="w-[100px] md:w-[160px]"
+              />
+            </Link>
+            <nav className="flex items-center space-x-4 text-center text-xs font-bold md:text-left md:text-sm">
+              {navigation.map((item) => (
+                <Link
+                  key={item.text}
+                  href={item.href}
+                  className="block rounded-md px-3 py-1 text-white transition hover:bg-amber-800"
+                >
+                  {item.text}
+                </Link>
+              ))}
+            </nav>
+            <Search />
+          </div>
+          <UserMenu />
+        </div>
+      </div>
+    </header>
+  )
+}
+const Footer = () => {
+  return (
+    <footer className="mt-auto  bg-[#222939] py-6">
+      <div className="container mx-auto flex items-center justify-between px-5">
+        <p className="text-sm font-bold text-white">Anno Stamps</p>
+
+        <div className="flex items-center space-x-5 px-5">
+          <a
+            href="https://www.buymeacoffee.com/miloK"
+            target="_blank"
+            className="text-white"
+          >
+            Support AnnoStamps
+          </a>
+          {socials.map((social) => (
+            <a
+              key={social.name}
+              href={social.url}
+              target="_blank"
+              data-testid={social.name}
+            >
+              <Image
+                src={social.src}
+                alt={social.name}
+                width={32}
+                height={32}
+                style={{
+                  maxWidth: '100%',
+                  height: 'auto',
+                }}
+              />{' '}
+            </a>
+          ))}
+        </div>
+      </div>
+    </footer>
+  )
+}
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
