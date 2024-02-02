5ba7899bf94adad6a85465a7bde61bc13d74a944
milank <milankornicer@gmail.com>
2024-02-02 10:28:40 -0500

    move layout to component index

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
 
+import UserMenu from '../Auth/UserMenu'
 import Search from '../Filter/Search'
 
 const navigation = [
