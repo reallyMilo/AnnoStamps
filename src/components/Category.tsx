c5ac8e87158c12263f5694b4e4f3b5edbf004f91
Milan K <57148574+mkcy3@users.noreply.github.com>
2024-02-03 16:28:21 -0500

    chore: set type on categoryMap (#218)

--- src/components/Category.tsx
+++ src/components/Category.tsx
@@ -6,12 +6,16 @@ import {
   TagIcon,
 } from '@heroicons/react/24/solid'
 
+import { CATEGORIES } from '@/lib/game/1800/data'
+
 type CategoryInfo = {
   color: string
   icon: React.ReactNode
 }
 
-const categoryMap: Record<string, CategoryInfo> = {
+type CategoryValues = (typeof CATEGORIES)[keyof typeof CATEGORIES]
+
+const categoryMap: Record<CategoryValues, CategoryInfo> = {
   housing: { icon: <HomeIcon className="h-5 w-5" />, color: 'bg-[#8B6834]' },
   production: { icon: <CogIcon className="h-5 w-5" />, color: 'bg-[#18806D]' },
   cosmetic: {
@@ -26,7 +30,7 @@ const categoryMap: Record<string, CategoryInfo> = {
 }
 
 const Category = ({ category = 'general' }: { category: string }) => {
-  const { icon, color } = categoryMap[category]
+  const { icon, color } = categoryMap[category as CategoryValues]
   return (
     <span
       className={`${color} flex w-fit items-center gap-1 rounded-full py-1 pl-2 pr-3 text-xs capitalize text-white`}
