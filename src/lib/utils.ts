325103aa0378008caec20e466b7f7d1ded3ba79e
milank <milankornicer@gmail.com>
2024-02-07 20:35:20 -0500

    chore: colocate and remove dead code

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -8,32 +8,3 @@ export function cn(...inputs: ClassValue[]) {
 export const stampsPerPage = () => {
   return Number(process.env.NEXT_PUBLIC_STAMPS_PER_PAGE) || 20
 }
-
-export const fetcher = async (url: string) => {
-  const res = await fetch(url)
-
-  if (!res.ok) {
-    const error = new Error(
-      'An error occurred while fetching the data.'
-    ) as Error & { info: string; status: number }
-    error.info = await res.json()
-    error.status = res.status
-    throw error
-  }
-
-  return res.json()
-}
-
-export async function sendRequest(
-  url: string,
-  {
-    arg,
-  }: {
-    arg: any
-  }
-) {
-  return fetch(url, {
-    method: 'POST',
-    body: arg,
-  }).then((res) => res.json())
-}
