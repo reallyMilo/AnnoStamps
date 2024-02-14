7399745b332b6d7a7a892d28f29be4aa53504a3f
Milan K <57148574+mkcy3@users.noreply.github.com>
2024-02-14 11:52:01 -0500

    signin auth  (#236)
    * migrate api/auth
    
    * auth/signin page to app

--- src/app/auth/signin/page.tsx
+++ src/app/auth/signin/page.tsx
@@ -1,43 +1,26 @@
-import type {
-  GetServerSidePropsContext,
-  InferGetServerSidePropsType,
-} from 'next'
 import Image from 'next/image'
 import Link from 'next/link'
-import { getServerSession } from 'next-auth/next'
-import { getProviders, signIn } from 'next-auth/react'
+import { redirect } from 'next/navigation'
 
+import { auth, signIn } from '@/auth'
 import Container from '@/components/ui/Container'
 
-import { authOptions } from '../api/auth/[...nextauth]'
-
-export async function getServerSideProps(context: GetServerSidePropsContext) {
-  const session = await getServerSession(context.req, context.res, authOptions)
-
+const SignInPage = async ({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined }
+}) => {
+  const session = await auth()
   if (session) {
-    return { redirect: { destination: '/' } }
+    redirect('/user/stamps')
   }
-  const providers = await getProviders()
-  const { query } = context
-
-  const redirect =
-    typeof query.callbackUrl === 'string' &&
-    query.callbackUrl?.startsWith('/stamp/')
-      ? query.callbackUrl
+  const redirectTo =
+    typeof searchParams.callbackUrl === 'string' &&
+    searchParams.callbackUrl?.startsWith('/stamp/')
+      ? searchParams.callbackUrl
       : '/user/stamps'
-
-  return {
-    props: {
-      providers,
-      redirect,
-    },
-  }
-}
-
-const SignInPage = ({
-  providers,
-  redirect,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
+  //https://github.com/nextauthjs/next-auth/issues/9293#issuecomment-1834443691
+  const providers = ['discord', 'google']
   return (
     <Container className="flex max-w-md flex-col justify-center">
       <Link href="/" className="mx-auto">
@@ -57,15 +40,17 @@ const SignInPage = ({
       <h3 className="mt-6 text-center text-2xl font-bold">Welcome!</h3>
 
       <div className="mt-10 flex flex-col space-y-4 border">
-        {providers &&
-          Object.values(providers).map((provider) => (
-            <button
-              key={provider.name}
-              onClick={() => signIn(provider.id, { callbackUrl: redirect })}
-              className="flex h-[46px] w-full items-center justify-center space-x-2 rounded-md border bg-white p-2 text-gray-500 transition-colors hover:border-gray-400 hover:bg-gray-50 hover:text-gray-600 focus:outline-none focus:ring-4 focus:ring-gray-400 focus:ring-opacity-25 disabled:cursor-not-allowed disabled:opacity-50 disabled:hover:border-gray-200 disabled:hover:bg-transparent disabled:hover:text-gray-500"
-            >
+        {Object.values(providers).map((provider) => (
+          <form
+            key={provider}
+            action={async () => {
+              'use server'
+              await signIn(provider, { redirectTo })
+            }}
+          >
+            <button className="flex h-[46px] w-full items-center justify-center space-x-2 rounded-md border bg-white p-2 text-gray-500 transition-colors hover:border-gray-400 hover:bg-gray-50 hover:text-gray-600 focus:outline-none focus:ring-4 focus:ring-gray-400 focus:ring-opacity-25 disabled:cursor-not-allowed disabled:opacity-50 disabled:hover:border-gray-200 disabled:hover:bg-transparent disabled:hover:text-gray-500">
               <Image
-                src={`/${provider.id}.svg`}
+                src={`/${provider}.svg`}
                 alt="discord sign in"
                 width={32}
                 height={32}
@@ -74,9 +59,10 @@ const SignInPage = ({
                   height: 'auto',
                 }}
               />
-              <span>Sign in with {provider.name}</span>
+              <span>Sign in with {provider}</span>
             </button>
-          ))}
+          </form>
+        ))}
       </div>
       <p className="mt-4">
         Please contact us on Discord to transfer your Stamps made with email
