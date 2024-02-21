61ab7df6eb98b88e1390a514e76a02695e685e69
milank <milankornicer@gmail.com>
2024-02-21 09:26:48 -0500

    migrate webhook to app route

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -1,41 +1,40 @@
 import { createHmac } from 'node:crypto'
 
-import { NextApiRequest, NextApiResponse } from 'next'
+import type { NextRequest } from 'next/server'
 
 import prisma from '@/lib/prisma/singleton'
 
-interface Req extends NextApiRequest {
-  body: {
-    [key: string]: string
-    id: string
-  }
-}
-export default async function webhookHandler(req: Req, res: NextApiResponse) {
-  if (!req.headers.signature || typeof req.headers.signature !== 'string') {
-    return res.status(401).json({ message: 'no signature' })
+export const POST = async (request: NextRequest) => {
+  const requestHeaders = new Headers(request.headers)
+
+  if (!requestHeaders.has('signature')) {
+    return Response.json({ message: 'No signature' }, { status: 400 })
   }
 
-  const sig = req.headers.signature
-
+  const sig = requestHeaders.get('signature')
+  const text = await request.text()
   const digest = createHmac('sha256', process.env.WEBHOOK_SECRET as string)
-    .update(JSON.stringify(req.body))
+    .update(text)
     .digest('hex')
 
   if (digest !== sig) {
-    return res.status(401).send({
-      message: `No signature match`,
-    })
+    return Response.json(
+      {
+        message: `No signature match`,
+      },
+      { status: 401 }
+    )
   }
 
-  const { id, ...url } = req.body
+  const { id, ...url } = JSON.parse(text)
 
   try {
     await prisma.image.update({
       where: { id },
       data: url,
     })
-    return res.status(200).json({ message: 'updated image' })
+    return Response.json({ message: 'updated image' }, { status: 200 })
   } catch (e) {
-    return res.status(500).json({ message: e })
+    return Response.json({ message: e }, { status: 500 })
   }
 }
