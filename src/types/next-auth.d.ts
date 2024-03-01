44c355141d91dc138814dce3ef900bc8fab78177
Milan K <57148574+mkcy3@users.noreply.github.com>
2024-03-01 17:42:52 -0500

    fix: incorrect next-auth types (#272)

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,19 +1,18 @@
 import { DefaultSession, DefaultUser } from 'next-auth'
 
+import type { UserWithStamps } from '@/lib/prisma/queries'
 declare module 'next-auth' {
   /**
    * Returned by `useSession`, `getSession` and received as a prop on the `SessionProvider` React Context
    */
   interface Session {
-    user: {
-      biography?: string
-      id: string
-      username?: string
-    } & DefaultSession['user']
+    user: Pick<
+      UserWithStamps,
+      'id' | 'biography' | 'username' | 'usernameURL'
+    > &
+      DefaultSession['user']
   }
-  interface User extends DefaultUser {
-    biography: string
-    discord: string
-    username: string
-  }
+  interface User
+    extends Pick<UserWithStamps, 'biography' | 'username' | 'usernameURL'>,
+      DefaultUser {}
 }
