ff382271e3809e23e8b761c6b70f2fc846723841
milank <milankornicer@gmail.com>
2024-03-04 12:12:03 -0500

    fix: menu text align and remove down arrow icon

--- src/components/Auth/UserMenu.tsx
+++ src/components/Auth/UserMenu.tsx
@@ -1,5 +1,4 @@
 import { Menu, Transition } from '@headlessui/react'
-import { ChevronDownIcon } from '@heroicons/react/20/solid'
 import {
   ArrowLeftIcon,
   HomeIcon,
@@ -32,11 +31,6 @@ const menuItems = [
   //   icon: HeartIcon,
   //   href: "/favorites",
   // },
-  {
-    label: 'Logout',
-    icon: ArrowLeftIcon,
-    href: null,
-  },
 ]
 const Avatar = ({ src }: { src: string | undefined | null }) => {
   if (!src) {
@@ -49,10 +43,7 @@ const UserMenu = () => {
   const { data: session, status } = useSession()
   const user = session?.user
 
-  if (status === 'loading')
-    return <div className="h-8 w-[75px] animate-pulse rounded-md bg-gray-200" />
-
-  if (!user)
+  if (status === 'unauthenticated')
     return (
       <Link
         href="/auth/signin"
@@ -61,13 +52,15 @@ const UserMenu = () => {
         Add Stamp
       </Link>
     )
+  if (status === 'loading')
+    return <div className="h-8 w-[75px] animate-pulse rounded-md bg-gray-200" />
+
   return (
     <Menu as="div" data-testid="user-menu" className="relative z-50">
       <Menu.Button className="group flex items-center space-x-px">
         <div className="relative flex h-9 w-9 shrink-0 items-center justify-center overflow-hidden rounded-full bg-gray-200">
           <Avatar src={user?.image} />
         </div>
-        <ChevronDownIcon className="h-5 w-5 shrink-0 text-white group-hover:text-white" />
       </Menu.Button>
       <Transition
         as={Fragment}
@@ -82,7 +75,7 @@ const UserMenu = () => {
           data-testid="user-dropdown"
           className="absolute right-0 mt-1 w-72 origin-top-right divide-y divide-gray-100 overflow-hidden rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
         >
-          <div className="mb-2 flex items-center space-x-2 px-4 py-4">
+          <div className="flex items-center space-x-2 px-4 py-4">
             <div className="relative flex h-9 w-9 shrink-0 items-center justify-center overflow-hidden rounded-full bg-gray-200">
               <Avatar src={user?.image} />
             </div>
@@ -102,33 +95,25 @@ const UserMenu = () => {
 
           <div className="py-2">
             {menuItems.map(({ label, href, icon: Icon }) => (
-              <div
-                key={label}
-                className="px-2 last:mt-2 last:border-t last:pt-2"
-                data-testid="user-menu-item"
-              >
-                <Menu.Item>
-                  {href ? (
-                    <Link
-                      href={href}
-                      className="flex items-center space-x-2 rounded-md px-4 py-2 hover:bg-gray-100"
-                    >
-                      <Icon className="h-5 w-5 shrink-0 text-gray-500" />
-                      <span>{label}</span>
-                    </Link>
-                  ) : (
-                    <button
-                      data-testid="logout-button"
-                      className="flex w-full items-center space-x-2 rounded-md px-4 py-2 hover:bg-gray-100"
-                      onClick={() => signOut({ callbackUrl: '/' })}
-                    >
-                      <Icon className="h-5 w-5 shrink-0 text-gray-500" />
-                      <span>{label}</span>
-                    </button>
-                  )}
-                </Menu.Item>
-              </div>
+              <Menu.Item key={label} as="div" data-testid="user-menu-item">
+                <Link
+                  href={href}
+                  className="flex items-center space-x-4 rounded-md px-6 py-2 hover:bg-gray-100"
+                >
+                  <Icon className="h-5 w-5 shrink-0 text-gray-500" />
+                  <span>{label}</span>
+                </Link>
+              </Menu.Item>
             ))}
+
+            <button
+              data-testid="logout-button"
+              className="mt-2 flex w-full items-center space-x-4 rounded-md border-t px-6 py-2 pt-2 hover:bg-gray-100"
+              onClick={() => signOut({ callbackUrl: '/' })}
+            >
+              <ArrowLeftIcon className="h-5 w-5 shrink-0 text-gray-500" />
+              <span>Logout</span>
+            </button>
           </div>
         </Menu.Items>
       </Transition>
