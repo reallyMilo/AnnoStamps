40a611fb18d580d915fb90d7d40bd9c0b4f619b1
Milan K <57148574+mkcy3@users.noreply.github.com>
2024-03-13 12:47:12 -0400

    test: fix type error on tests (#288)

--- src/__tests__/pages/HomePage.test.tsx
+++ src/__tests__/pages/HomePage.test.tsx
@@ -1,9 +1,20 @@
-import { stampMock } from '@/__mocks__/data'
+import type { StampWithRelations } from '@/lib/prisma/queries'
 import HomePage from '@/pages'
 
 import { render, screen } from '../test-utils'
 
-const stamps = stampMock
+const stamps = [
+  {
+    id: 'clmsefb5m00ujk9xxenynap1z',
+    createdAt: 1695253939,
+    changedAt: 1695253939,
+    user: {
+      username: 'user100',
+    },
+    images: [],
+  },
+] as unknown as StampWithRelations[]
+
 describe('Home Page', () => {
   it('render HomePage with filter and without stamps', () => {
     render(<HomePage count={0} stamps={[]} pageNumber={1} />)
