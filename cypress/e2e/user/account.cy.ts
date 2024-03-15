79a6f9f79d0302cd196b6fb805ef0779786fc78b
Milan K <57148574+mkcy3@users.noreply.github.com>
2024-03-15 18:29:08 -0400

    ci: e2e tests (#299)
    * add server-start-and-test package
    
    * add username to session
    
    * fix account page test
    
    * use dev instead of start
    
    * ci e2e test
    
    * run on current branch
    
    * PR targeting main only
    
    * script ci to ci:e2e
    
    * add env and composite build action
    
    * missing period on github path
    
    * bump actions/checkout@v4 and pnpm v3
    
    * actions/setup-node@v4
    
    * fix ci
    
    * fix node modules
    
    * docker db attempt 1
    
    * no docker test
    
    * use github container services
    
    * fix variable not found
    
    * needs both env
    
    * remove all files from docker attempt
    
    * switch to cypress-github action
    
    * dev not recognized
    
    * full build on e2e
    
    * change default to chrome

--- cypress/e2e/user/account.cy.ts
+++ cypress/e2e/user/account.cy.ts
@@ -1,27 +1,26 @@
 describe('user/account route', () => {
-  beforeEach(() => {
-    cy.userSession()
-    cy.visit('/user/account')
-    cy.wait('@session')
-  })
-
   it('new user can set username', () => {
-    cy.intercept('PUT', '/api/user/cypressTester', {
+    cy.visit('/user/account')
+    cy.intercept('PUT', '/api/user', {
       statusCode: 200,
-    }).as('setUsername')
-    cy.get('#username').type('cypressTester')
-    cy.get('#user-settings').submit()
-
+    })
     cy.intercept('/api/auth/session', {
       user: {
         name: 'cypress-tester',
         email: 'cypress@tester.com',
         image: null,
-        username: 'cypressTester',
+        username: null,
       },
       expires: '3000-01-01T00:00:00.000Z',
       accessToken: 'abcdefghijklmnopqrst',
     })
+    cy.get('#username').type('cypressTester')
+    cy.get('#user-settings').submit()
+
+    cy.userSession()
     cy.get('#username').invoke('val').should('equal', 'cypressTester')
+    cy.findByText(
+      'If you wish to change your username please contact us via the discord server.'
+    ).should('exist')
   })
 })
