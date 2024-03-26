c36cacd820c6fd3cce6df082175cbc94299e4d44
milank <milankornicer@gmail.com>
2024-03-26 11:11:08 -0400

    test: set-username e2e

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,108 @@
+import { PrismaClient } from '@prisma/client'
 import { defineConfig } from 'cypress'
+
+const prisma = new PrismaClient()
 
 export default defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
-    setupNodeEvents(on, config) {},
+    setupNodeEvents(on) {
+      on('task', {
+        async 'db:query'(rawQuery: string) {
+          const data = await prisma.$queryRawUnsafe(`${rawQuery}`)
+
+          return data
+        },
+        async 'db:seed'() {
+          // use main seed for now
+        },
+        async 'db:removeTestUser'() {
+          await prisma.image.delete({
+            where: {
+              id: 'testSeedStampImageId',
+            },
+          })
+
+          await prisma.stamp.delete({
+            where: {
+              id: 'testSeedUserStampId',
+            },
+          })
+
+          await prisma.session.deleteMany()
+          await prisma.account.deleteMany()
+          return await prisma.user.delete({
+            where: {
+              id: 'testSeedUserId',
+            },
+          })
+        },
+        async 'db:testUser'(setUsername?: boolean) {
+          const username = {
+            username: 'testSeedUser',
+            usernameURL: 'testseeduser',
+            biography: 'amazing test user bio',
+          }
+          const testSeedUser = {
+            id: 'testSeedUserId',
+            name: `testSeedUser`,
+            email: `testSeedUser@example.com`,
+            username: null,
+            usernameURL: null,
+            biography: null,
+            ...(setUsername ? username : null),
+          }
+
+          await prisma.user.create({
+            data: testSeedUser,
+          })
+          await prisma.account.create({
+            data: {
+              id: 'testSeedAccountId',
+              userId: 'testSeedUserId',
+              type: 'oauth',
+              provider: 'discord',
+              providerAccountId: '123213123123',
+              access_token: 'abcdefghijklmnopqrst',
+            },
+          })
+          await prisma.session.create({
+            data: {
+              id: 'testSeedUserSessionId',
+              sessionToken: 'cdc4b0fb-77b5-44b5-947a-dde785af2676',
+              userId: 'testSeedUserId',
+              expires: '3000-01-01T00:00:00.000Z',
+            },
+          })
+
+          return await prisma.stamp.create({
+            data: {
+              id: 'testSeedUserStampId',
+              userId: 'testSeedUserId',
+              game: '1800',
+              title: `Test-Seed-User-Stamp`,
+              description: `Test seed user stamp`,
+              category: 'cosmetic',
+              region: 'old world',
+              stampFileUrl: '/stamp.zip',
+              collection: true,
+              downloads: 123,
+              images: {
+                create: [
+                  {
+                    id: 'testSeedStampImageId',
+                    originalUrl:
+                      'https://placehold.co/2000x2000.png?text=Original',
+                    largeUrl: `https://placehold.co/1024x576.png?text=Large`,
+                    smallUrl: `https://placehold.co/500x281.png?text=Small`,
+                  },
+                ],
+              },
+            },
+          })
+        },
+      })
+    },
     video: false,
     chromeWebSecurity: false,
     env: {
--- cypress/global.d.ts
+++ cypress/global.d.ts
@@ -1,7 +1,8 @@
 /// <reference types="cypress" />
-
+/* eslint-disable @typescript-eslint/no-explicit-any */
 declare namespace Cypress {
   interface Chainable {
+    database(rawQuery: string, log?: boolean): Chainable<any>
     getBySel(
       dataTestAttribute: string,
       args?: any
