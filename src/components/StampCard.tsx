f501f8ad4ca7d078ae5bbc1b65c6f52f36fe5eb6
Milan K <57148574+mkcy3@users.noreply.github.com>
2024-04-23 20:58:06 -0400

    fix: text size and multiple link elements (#366)

--- src/components/StampCard.tsx
+++ src/components/StampCard.tsx
@@ -41,73 +41,71 @@ const StampCard = ({
       : images[0].smallUrl ?? images[0].originalUrl
 
   return (
-    <div className="rounded-lg bg-white shadow-md">
-      <Link
-        href={`/stamp/${id}`}
-        data-testid="stamp-card-link"
-        prefetch={false}
-      >
-        <div className="aspect-h-3 aspect-w-4 overflow-hidden rounded-tl-lg rounded-tr-lg bg-gray-200">
-          <Image
-            src={srcUrl}
-            alt={title ?? 'image alt'}
-            className="transition hover:opacity-80"
-            fill
-            sizes="(max-width: 320px) 700px
+    <div className="group relative rounded-lg bg-white shadow-md">
+      <div className="aspect-h-3 aspect-w-4 overflow-hidden rounded-tl-lg rounded-tr-lg bg-gray-200 group-hover:opacity-75 ">
+        <Image
+          src={srcUrl}
+          alt={title ?? 'image alt'}
+          className="transition hover:opacity-80"
+          fill
+          sizes="(max-width: 320px) 700px
                 (max-width: 768px) 390px,
                 (max-width: 1200px) 290px"
-            style={{
-              objectFit: 'cover',
-            }}
-          />
-          {modded && (
-            <div className="absolute right-2 top-2 ml-auto flex h-fit w-fit flex-row items-center rounded-full bg-accent px-2 py-1 text-xs text-default">
-              <WrenchIcon className="mr-1 h-3 w-3" />
-              mods
-            </div>
-          )}
-        </div>
-      </Link>
-      <div className="flex flex-col flex-nowrap gap-y-2 p-4">
-        <Link href={`/stamp/${id}`} prefetch={false}>
-          <div className="flex items-baseline justify-between">
-            <h4 id="stamp-region" className="text-accent">
-              {region}
-            </h4>
-            <div className="text-xs" suppressHydrationWarning>
-              {distanceUnixTimeToNow(createdAt)}
-            </div>
+          style={{
+            objectFit: 'cover',
+          }}
+        />
+        {modded && (
+          <div className="absolute right-2 top-2 ml-auto flex h-fit w-fit flex-row items-center rounded-full bg-accent px-2 py-1 text-xs text-default">
+            <WrenchIcon className="mr-1 h-3 w-3" />
+            mods
           </div>
+        )}
+      </div>
 
-          <h2
-            id="stamp-title"
-            className="line-clamp-2 min-h-[45px] w-full overflow-hidden text-ellipsis text-lg font-semibold leading-tight text-gray-700"
+      <div className="flex flex-col flex-nowrap gap-y-2 p-4">
+        <div className="flex items-baseline justify-between">
+          <h4 id="stamp-region" className="text-sm capitalize text-accent">
+            {region}
+          </h4>
+          <div className="text-sm" suppressHydrationWarning>
+            {distanceUnixTimeToNow(createdAt)}
+          </div>
+        </div>
+        <h2
+          id="stamp-title"
+          className="line-clamp-2 min-h-[45px] w-full overflow-hidden text-ellipsis text-lg font-semibold leading-tight text-gray-700"
+        >
+          <Link
+            href={`/stamp/${id}`}
+            data-testid="stamp-card-link"
+            prefetch={false}
           >
+            <span aria-hidden="true" className="absolute inset-0"></span>
             {title}
-          </h2>
-        </Link>
+          </Link>
+        </h2>
 
         {user?.username ? (
           <Link
             href={`/${user.usernameURL}`}
-            className="flex h-11 items-center gap-1 text-slate-500"
+            className="z-10 flex h-11 w-fit items-center gap-1 text-slate-500 hover:text-sky-700"
             prefetch={false}
           >
             <UserCircleIcon className="h-4 w-4" />
-            <span className="hover:text-sky-700">{user.username}</span>
+            {user.username}
           </Link>
         ) : (
-          <Link href={`/stamp/${id}`} className="h-11" prefetch={false} />
+          <span className="inline-block"></span>
         )}
-        <Link href={`/stamp/${id}`} prefetch={false}>
-          <div className="flex justify-between">
-            <StampCategoryIcon category={category} />
-            <div className="flex items-end">
-              <ArrowDownTrayIcon className="mr-2 inline-block h-5 w-5 self-center" />
-              <span data-testid="stamp-card-downloads">{downloads}</span>
-            </div>
+
+        <div className="flex justify-between">
+          <StampCategoryIcon category={category} />
+          <div className="flex items-end">
+            <ArrowDownTrayIcon className="mr-2 inline-block h-5 w-5 self-center" />
+            <span data-testid="stamp-card-downloads">{downloads}</span>
           </div>
-        </Link>
+        </div>
       </div>
     </div>
   )
