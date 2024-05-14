5db72641450f3af1606165f648a086380c4b055c
Milan K <57148574+mkcy3@users.noreply.github.com>
2024-05-14 11:01:49 -0400

    chore: upgrade headlessui/react 1.7.18 to 2.0.3 (#382)
    * chore: upgrade headlessui/react 1.7.18 to 2.0.3
    
    * stub ResizeObserver

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,6 +1,13 @@
 import '@testing-library/jest-dom'
 
 import { beforeAll, vi } from 'vitest'
+const ResizeObserverMock = vi.fn(() => ({
+  observe: vi.fn(),
+  unobserve: vi.fn(),
+  disconnect: vi.fn(),
+}))
+
+vi.stubGlobal('ResizeObserver', ResizeObserverMock)
 beforeAll(() => {
   vi.mock('next/router', () => require('next-router-mock'))
 })
