9aa05a9d499ba9c640dacd1a50c9cd46b2e56694
milank <milankornicer@gmail.com>
2024-05-21 19:41:19 -0400

    update import

--- src/components/Filter/__tests__/Pagination.test.tsx
+++ src/components/Filter/__tests__/Pagination.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen } from '../../__tests__/test-utils'
+import { render, screen } from '@/__tests__/test-utils'
 import { Pagination } from '../Pagination'
 
 describe('Pagination', () => {
