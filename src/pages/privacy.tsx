fc84adde35055564360de063077fd3e5da585ca4
milank <milankornicer@gmail.com>
2024-05-28 17:39:19 -0400

    refactor: privacy page to use ui components

--- src/pages/privacy.tsx
+++ src/pages/privacy.tsx
@@ -1,18 +1,18 @@
-import { Container } from '@/components/ui'
+import { Container, Heading, Strong, Subheading, Text } from '@/components/ui'
 
 const Privacy = () => {
   return (
     <Container className="prose space-y-2">
-      <h1>Privacy Policy</h1>
+      <Heading>Privacy Policy</Heading>
 
-      <h2>Anno Stamps Privacy Policy</h2>
-      <p>
+      <Subheading>Anno Stamps Privacy Policy</Subheading>
+      <Text>
         This Privacy Policy describes how your personal information is
         collected, used, and shared when you visit https://annostamps.com , or
         any of our product demo subdomains, (the “Site”).
-      </p>
-      <h3>PERSONAL INFORMATION WE COLLECT</h3>
-      <p>
+      </Text>
+      <Subheading level={3}>PERSONAL INFORMATION WE COLLECT</Subheading>
+      <Text>
         When you visit the Site, we automatically collect certain information
         about your device, including information about your web browser, IP
         address, time zone, and some of the cookies that are installed on your
@@ -21,13 +21,13 @@ const Privacy = () => {
         or search terms referred you to the Site, and information about how you
         interact with the Site. We refer to this automatically-collected
         information as “Device Information.”
-      </p>
-      <p>
-        <strong>
+      </Text>
+      <Text>
+        <Strong>
           We collect Device Information using the following technologies:
-        </strong>
-      </p>
-      <ul className="list-inside list-disc">
+        </Strong>
+      </Text>
+      <ul className="list-inside list-disc text-midnight dark:text-white">
         <li>
           “Cookies” are data files that are placed on your device or computer
           and often include an anonymous unique identifier. For more information
@@ -45,25 +45,27 @@ const Privacy = () => {
         </li>
       </ul>
 
-      <p>
+      <Text>
         Additionally when you subscribe to our newsletter we collect your name
         and email address.
-      </p>
-      <h3>HOW DO WE USE YOUR PERSONAL INFORMATION?</h3>
-      <p>
+      </Text>
+      <Subheading level={3}>
+        HOW DO WE USE YOUR PERSONAL INFORMATION?
+      </Subheading>
+      <Text>
         We use your name and email address to communicate with you and to
         provide you with information or advertising relating to our products or
         services.
-      </p>
-      <p>
+      </Text>
+      <Text>
         We use the Device Information that we collect to help us screen for
         potential risk to the Site (in particular, your IP address), and more
         generally to improve and optimize our Site (for example, by generating
         analytics about how our customers browse and interact with the Site, and
         to assess the success of our marketing and advertising campaigns).
-      </p>
-      <h3>SHARING YOUR PERSONAL INFORMATION</h3>
-      <p>
+      </Text>
+      <Subheading level={3}>SHARING YOUR PERSONAL INFORMATION</Subheading>
+      <Text>
         We share your Personal Information with third parties, such as Google
         Analytics, to help us use your Personal Information, as described above.
         &nbsp;We use Google Analytics to help us understand how our customers
@@ -72,23 +74,23 @@ const Privacy = () => {
         &nbsp;https://www.google.com/intl/en/policies/privacy/. You can also
         opt-out of Google Analytics here:
         https://tools.google.com/dlpage/gaoptout.
-      </p>
-      <p>
+      </Text>
+      <Text>
         Finally, we may also share your Personal Information to comply with
         applicable laws and regulations, to respond to a subpoena, search
         warrant or other lawful request for information we receive, or to
         otherwise protect our rights.
-      </p>
-      <p>
+      </Text>
+      <Text>
         As described above, we use your Personal Information to provide you with
         targeted advertisements or marketing communications we believe may be of
         interest to you. For more information about how targeted advertising
         works, you can visit the Network Advertising Initiative’s (“NAI”)
         educational page at
         http://www.networkadvertising.org/understanding-online-advertising/how-does-it-work.
-      </p>
-      <p>You can opt out of targeted advertising by:</p>
-      <ul className="list-inside list-disc">
+      </Text>
+      <Text>You can opt out of targeted advertising by:</Text>
+      <ul className="list-inside list-disc text-midnight dark:text-white">
         <li>FACEBOOK – https://www.facebook.com/settings/?tab=ads</li>
         <li>GOOGLE – https://www.google.com/settings/ads/anonymous</li>
         <li>
@@ -97,44 +99,44 @@ const Privacy = () => {
         </li>
       </ul>
 
-      <h3>DO NOT TRACK</h3>
-      <p>
+      <Subheading level={3}>DO NOT TRACK</Subheading>
+      <Text>
         Please note that we do not alter our Site’s data collection and use
         practices when we see a Do Not Track signal from your browser.
-      </p>
-      <h3>YOUR RIGHTS</h3>
-      <p>
+      </Text>
+      <Subheading level={3}>YOUR RIGHTS</Subheading>
+      <Text>
         If you are a European resident, you have the right to access personal
         information we hold about you and to ask that this information be
         corrected, updated, or deleted. If you would like to exercise this
         right, please contact us through the contact information below.
-      </p>
-      <p>
+      </Text>
+      <Text>
         Additionally, if you are a European resident we note that we are
         processing your information in order to pursue our legitimate business
         interests listed above. Additionally, please note that your information
         will be transferred outside of Europe, including to Canada and the
         United States.
-      </p>
-      <h3>DATA RETENTION</h3>
-      <p>
+      </Text>
+      <Subheading level={3}>DATA RETENTION</Subheading>
+      <Text>
         When you subscribe to our newsletter through the Site, we will maintain
         your information for our records unless and until you ask us to delete
         this information.
-      </p>
-      <h3>CHANGES</h3>
-      <p>
+      </Text>
+      <Subheading level={3}>CHANGES</Subheading>
+      <Text>
         We may update this privacy policy from time to time in order to reflect,
         for example, changes to our practices or for other operational, legal or
         regulatory reasons.
-      </p>
-      <h3>CONTACT US</h3>
-      <p>
+      </Text>
+      <Subheading level={3}>CONTACT US</Subheading>
+      <Text>
         For more information about our privacy practices, if you have questions,
         would like to update or remove your personal information, or if you
         would like to make a complaint, please contact us by e-mail at
         annostampsite@gmail.com.&nbsp;
-      </p>
+      </Text>
     </Container>
   )
 }
