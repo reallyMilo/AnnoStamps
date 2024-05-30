7f5aeacc7ba2bf1954ab0cc2370732e3015e127f
milank <milankornicer@gmail.com>
2024-05-29 22:29:59 -0400

    fix: pagination not rendering pages correctly and use new ui component

--- src/components/Filter/Pagination.tsx
+++ src/components/Filter/Pagination.tsx
@@ -1,29 +1,59 @@
-import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/20/solid'
 import { useEffect } from 'react'
 
+import {
+  Pagination as PaginationRoot,
+  PaginationGap,
+  PaginationList,
+  PaginationNext,
+  PaginationPage,
+  PaginationPrevious,
+} from '@/components/ui'
 import { STAMPS_PER_PAGE } from '@/lib/constants'
 import { useQueryParams } from '@/lib/hooks/useQueryParams'
-import { cn } from '@/lib/utils'
-
-const generatePageNumbers = (totalPageCount: number, currentPage: number) => {
-  const presetMaxPageList = 20
-  if (totalPageCount <= presetMaxPageList) {
-    return Array.from({ length: totalPageCount }, (_, i) => i + 1)
-  } else {
-    const middlePage = Math.ceil(presetMaxPageList / 2)
-    const startPage = Math.max(1, currentPage - middlePage + 1)
-    const endPage = Math.min(startPage + presetMaxPageList - 1, totalPageCount)
-
-    return Array.from(
-      { length: endPage - startPage + 1 },
-      (_, i) => startPage + i
-    )
-  }
-}
 
 type PaginationProps = {
   count: number
   page: number
+}
+
+const maxPaginationList = 11
+
+// paginate that does not shift number of elements
+const paginate = (
+  totalPageCount: number,
+  currentPage: number
+): (number | null)[] => {
+  if (totalPageCount <= maxPaginationList + 2) {
+    return Array.from({ length: totalPageCount }, (_, i) => i + 1)
+  }
+  const range = Math.floor(maxPaginationList / 2)
+  const isLeftGap = currentPage - range - 1 > 2
+  const isRightGap = totalPageCount - currentPage - range - 1 >= 2
+  const pages: (number | null)[] = [1]
+
+  let leftStartNumber
+  if (isLeftGap && isRightGap) {
+    leftStartNumber = currentPage - range + 1
+  } else if (!isLeftGap) {
+    leftStartNumber = 2
+  } else {
+    // fix: some edge cases with totalPageCount near range limit
+    leftStartNumber = totalPageCount - maxPaginationList + 1
+  }
+
+  for (let index = 1, inc = 0; index <= maxPaginationList; index++) {
+    if (
+      (isLeftGap && index === 1) ||
+      (isRightGap && index === maxPaginationList)
+    ) {
+      pages.push(null)
+    } else {
+      pages.push(leftStartNumber + inc)
+      inc++
+    }
+  }
+  pages.push(totalPageCount)
+  return pages
 }
 
 export const Pagination = ({ count, page }: PaginationProps) => {
@@ -35,85 +65,27 @@ export const Pagination = ({ count, page }: PaginationProps) => {
     }
   })
   const totalPageCount = Math.ceil(count / STAMPS_PER_PAGE)
-
-  const pageNumbers = generatePageNumbers(totalPageCount, page)
-
-  const incrementPage = () => {
-    if (page + 1 > totalPageCount) return
-
-    setQuery({ payload: (page + 1).toString(), type: 'page' })
-  }
-  const decrementPage = () => {
-    if (page - 1 < 1) return
-    setQuery({ payload: (page - 1).toString(), type: 'page' })
-  }
-  const changePage = (e: React.MouseEvent<HTMLLIElement>) => {
-    const page = e.currentTarget.value.toString()
-
-    setQuery({ payload: page, type: 'page' })
-  }
-  const starting = (page - 1) * STAMPS_PER_PAGE + 1
-  const ending = Math.min(starting + STAMPS_PER_PAGE - 1, count)
-
+  const pageNumbers = paginate(totalPageCount, page)
   return (
-    <div className="mt-6 flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
-      <div className="flex flex-1 justify-between sm:hidden">
-        <button
-          className="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
-          onClick={decrementPage}
-        >
-          Previous
-        </button>
-        <button
-          className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
-          onClick={incrementPage}
-        >
-          Next
-        </button>
-      </div>
-      <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
-        <div>
-          <p className="text-sm text-gray-700">
-            Showing <span className="font-medium">{starting}</span> to{' '}
-            <span className="font-medium">{ending}</span> of{' '}
-            <span className="font-medium">{count}</span> results
-          </p>
-        </div>
-        <div>
-          <nav aria-label="Pagination">
-            <ul className="isolate inline-flex cursor-pointer -space-x-px rounded-md shadow-sm">
-              <li
-                onClick={decrementPage}
-                className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-              >
-                <span className="sr-only">Previous</span>
-                <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
-              </li>
-              {pageNumbers.map((pageNum) => (
-                <li
-                  key={pageNum + '-pagination'}
-                  className={cn(
-                    'relative hidden items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-indigo-400 focus:z-20 focus:outline-offset-0 md:inline-flex',
-                    pageNum === page &&
-                      'z-10 bg-indigo-600 text-white hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'
-                  )}
-                  value={pageNum}
-                  onClick={changePage}
-                >
-                  {pageNum}
-                </li>
-              ))}
-              <li
-                onClick={incrementPage}
-                className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
-              >
-                <span className="sr-only">Next</span>
-                <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
-              </li>
-            </ul>
-          </nav>
-        </div>
-      </div>
-    </div>
+    <PaginationRoot>
+      <PaginationPrevious href={page === 1 ? null : '1'} />
+      <PaginationList>
+        {pageNumbers.map((pageNum, idx) => {
+          if (!pageNum) {
+            return <PaginationGap key={`gap-${idx}-pagination`} />
+          }
+          return (
+            <PaginationPage
+              href={'20'}
+              key={pageNum + '-pagination'}
+              current={pageNum === page}
+            >
+              {pageNum}
+            </PaginationPage>
+          )
+        })}
+      </PaginationList>
+      <PaginationNext href={page === totalPageCount ? null : '10'} />
+    </PaginationRoot>
   )
 }
--- src/components/Filter/__tests__/Pagination.test.tsx
+++ src/components/Filter/__tests__/Pagination.test.tsx
@@ -3,10 +3,33 @@ import { render, screen } from '@/__tests__/test-utils'
 import { Pagination } from '../Pagination'
 
 describe('Pagination', () => {
-  it('renders with provided props', () => {
-    render(<Pagination count={3000} page={1} />)
+  it('renders when below max size', () => {
+    render(<Pagination count={100} page={1} />)
 
-    expect(screen.getByText('3000')).toBeInTheDocument()
-    expect(screen.getByText('19')).toBeInTheDocument()
+    expect(screen.getByRole('button', { name: 'Previous page' })).toBeDisabled()
+    expect(screen.getByRole('link', { current: 'page' })).toHaveTextContent('1')
+    expect(screen.getAllByRole('link', { name: /Page \d+/ })).toHaveLength(5)
+    expect(screen.getByRole('link', { name: 'Next page' })).toBeInTheDocument()
+  })
+  it('renders when exceeds max size', () => {
+    render(<Pagination count={500} page={1} />)
+
+    expect(screen.getAllByRole('link', { name: /Page \d+/ })).toHaveLength(12)
+    expect(screen.getByTestId('pagination-gap')).toBeInTheDocument()
+  })
+  it('renders two gap indicators for middle page with correct page range', () => {
+    const currentPage = 25
+    render(<Pagination count={1000} page={currentPage} />)
+
+    expect(screen.getByRole('link', { current: 'page' })).toHaveTextContent(
+      '25'
+    )
+    const allLinks = screen.getAllByRole('link', { name: /Page \d+/ })
+    expect(allLinks).toHaveLength(11)
+
+    expect(allLinks[1]).toHaveTextContent((currentPage - 4).toString())
+    expect(allLinks[9]).toHaveTextContent((currentPage + 4).toString())
+
+    expect(screen.getAllByTestId('pagination-gap')).toHaveLength(2)
   })
 })
