588dcca0f70b68286f41098566fc4fc2ea5b5674
milank <milankornicer@gmail.com>
2024-06-07 10:39:33 -0400

    orderBy desc

--- cypress/e2e/create-stamp.cy.ts
+++ cypress/e2e/create-stamp.cy.ts
@@ -175,7 +175,7 @@ describe('Stamp creation', () => {
 
     cy.findByText('cypress test title').should('be.visible')
     cy.getBySel('stamp-card-link')
-      .last()
+      .first()
       .invoke('attr', 'href')
       .should('be.a', 'string')
       .invoke('split', '/')
