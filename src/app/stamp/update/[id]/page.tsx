82e7082b6415613411a2e6b76d78d997ebcb1c00
milank <milankornicer@gmail.com>
2024-06-18 20:10:24 -0400

    refactor: update stamp to app

--- src/app/stamp/update/[id]/page.tsx
+++ src/app/stamp/update/[id]/page.tsx
@@ -1,115 +1,20 @@
 import { ExclamationTriangleIcon } from '@heroicons/react/20/solid'
-import JSZip, { type JSZipObjectWithData } from 'jszip'
 import Link from 'next/link'
-import { useRouter } from 'next/router'
-import { useSession } from 'next-auth/react'
-import { useMemo } from 'react'
-import useSWR from 'swr'
+import { redirect } from 'next/navigation'
 
-import { StampForm } from '@/components/StampForm/StampForm'
-import { Container, Heading, Text } from '@/components/ui'
-import type { UserWithStamps } from '@/lib/prisma/queries'
+import { auth } from '@/auth'
+import { Container, Text } from '@/components/ui'
+import { userIncludeStatement } from '@/lib/prisma/queries'
+import prisma from '@/lib/prisma/singleton'
 
-const useUserStamps = () => {
-  const router = useRouter()
-  const { data: session, status } = useSession({
-    required: true,
-    onUnauthenticated() {
-      router.replace('/auth/signin')
-    },
-  })
+import { UpdateStampForm } from './UpdateStampForm'
 
-  const { data, isLoading, error } = useSWR<{ data: UserWithStamps }>(
-    status === 'authenticated' ? '/api/user' : null,
-    async (url: string) => {
-      const res = await fetch(url)
+const EditStampPage = async ({ params }: { params: { id: string } }) => {
+  const session = await auth()
 
-      if (!res.ok) {
-        const error = new Error(
-          'An error occurred while fetching the data.'
-        ) as Error & { info: string; status: number }
-        error.info = await res.json()
-        error.status = res.status
-        throw error
-      }
-
-      return res.json()
-    }
-  )
-
-  return {
-    userStamps: data?.data,
-    session,
-    isLoading,
-    error,
+  if (!session) {
+    redirect('/auth/signin')
   }
-}
-
-type Stamp = UserWithStamps['listedStamps'][0]
-
-const EditStampPage = () => {
-  const { query } = useRouter()
-
-  const { session, userStamps, isLoading, error } = useUserStamps()
-
-  const stamp = useMemo<Stamp | undefined>(() => {
-    if (!userStamps) {
-      return undefined
-    }
-    return userStamps.listedStamps.find((stamp) => stamp.id === query.id)
-  }, [userStamps, query.id])
-
-  const { data: stampZip, isLoading: isStampLoading } = useSWR(
-    stamp ? stamp.stampFileUrl : null,
-    async (url: string) => {
-      const res = await fetch(url)
-      const blob = await res.blob()
-      const zip = await JSZip.loadAsync(blob)
-      return zip
-    }
-  )
-  const handleOnSubmit = async (
-    formData: FormData,
-    addImages: string[],
-    imagesToRemove?: string[]
-  ) => {
-    if (!stamp) {
-      throw new Error('no stamp')
-    }
-
-    const res = await fetch(`/api/stamp/update/${stamp.id}`, {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        ...Object.fromEntries(formData),
-        addImages,
-        deleteImages: imagesToRemove ?? [],
-      }),
-    })
-
-    return res
-  }
-  if (error) {
-    return (
-      <Container>
-        <Heading>Error</Heading>
-        <Text>{error.info.message}</Text>
-      </Container>
-    )
-  }
-  if (isLoading || isStampLoading) {
-    return (
-      <Container>
-        <Heading className="text-xl font-bold text-gray-800">
-          Fetching Stamp
-        </Heading>
-        <div className="h-8 w-[75px] animate-pulse rounded-md bg-gray-200" />{' '}
-      </Container>
-    )
-  }
-  //TODO: Page Wrapper that checks auth + set username for all routes that require auth and username set
 
   if (!session?.user.username) {
     return (
@@ -137,27 +42,26 @@ const EditStampPage = () => {
     )
   }
 
+  const userStamp = await prisma.user.findUnique({
+    include: userIncludeStatement,
+    where: {
+      id: session.user.id,
+      listedStamps: {
+        some: {
+          id: {
+            equals: params.id,
+          },
+        },
+      },
+    },
+  })
+  if (!userStamp) {
+    throw new Error('Not your stamp')
+  }
+
   return (
     <Container className="md:max-w-5xl">
-      {stamp && stampZip ? (
-        <StampForm.Root
-          stamp={stamp}
-          zipFiles={Object.values(stampZip.files) as JSZipObjectWithData[]}
-        >
-          <StampForm.Form onSubmit={handleOnSubmit}>
-            <StampForm.Header
-              title="Edit your stamp"
-              subTitle="Fill out the form below to update your stamp."
-            />
-            <StampForm.ImageUpload />
-            <StampForm.FileUpload />
-            <StampForm.StampInfoFieldGroup />
-            <StampForm.Submit> Update Stamp </StampForm.Submit>
-          </StampForm.Form>
-        </StampForm.Root>
-      ) : (
-        <Text> not your stamp </Text>
-      )}
+      <UpdateStampForm stamp={userStamp.listedStamps[0]} />
     </Container>
   )
 }
