ea8a66279e850e7cbb9c768a7fcf1533033efccb
Milan K <57148574+mkcy3@users.noreply.github.com>
2024-06-24 13:31:33 -0400

    chore: loading skeleton for stamp page (#509)

---
+++ src/app/stamp/[id]/loading.tsx
@@ -0,0 +1,17 @@
+import { Container } from '@/components/ui'
+
+const StampPageLoading = () => {
+  return (
+    <Container className="max-w-5xl animate-pulse space-y-6 px-0">
+      {/* Image */}
+      <div className="min-h-[576px] w-full bg-gray-500"></div>
+      <div className="space-y-6 px-2 text-midnight sm:px-0 dark:text-white">
+        <div className="min-h-8 w-1/3 bg-gray-500"></div>
+        <div className="min-h-8 bg-gray-500"></div>
+        <div className="min-h-40 bg-gray-500"></div>
+      </div>
+    </Container>
+  )
+}
+
+export default StampPageLoading
