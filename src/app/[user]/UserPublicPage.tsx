77f134f830646accef7a5336749073c2a4b383bb
milank <milankornicer@gmail.com>
2024-07-19 18:31:00 -0400

    refactor: simplify username page

--- src/app/[user]/UserPublicPage.tsx
+++ src/app/[user]/UserPublicPage.tsx
@@ -2,51 +2,38 @@ import { StampCard } from '@/components/StampCard'
 import { Container, Grid, Heading, Text } from '@/components/ui'
 import type { UserWithStamps } from '@/lib/prisma/models'
 
-type UserBannerProps = {
-  stampCount: number
-} & Pick<UserWithStamps, 'biography' | 'username'>
-
-const UserPublicBanner = ({
-  username,
+export const UserPublicPage = ({
   biography,
-  stampCount,
-}: UserBannerProps) => {
-  return (
-    <div className="mb-4 flex flex-col gap-y-2 border-b-2 pb-10">
-      <div className="flex space-x-4">
-        <Heading>{username}</Heading>
-        {stampCount > 0 && (
-          <Text className="self-end">{stampCount} Stamps</Text>
-        )}
-      </div>
-      <Text className="text-sm">{biography}</Text>
-    </div>
-  )
-}
-export const UserPublicPage = ({ user }: { user: UserWithStamps }) => {
-  const { username, biography } = user
-  const userBannerProps = {
-    username,
-    biography,
-    stampCount: user.listedStamps.length,
-  }
-
-  if (user.listedStamps.length === 0) {
-    return (
-      <Container>
-        <UserPublicBanner {...userBannerProps} />
-        <Text>User has no stamps</Text>
-      </Container>
-    )
-  }
+  listedStamps,
+  image,
+  usernameURL,
+  username,
+}: UserWithStamps) => {
+  const stampCount = listedStamps.length
 
   return (
     <Container>
-      <UserPublicBanner {...userBannerProps} />
+      <div className="mb-4 flex flex-col gap-y-2 border-b-2 pb-10">
+        <div className="flex space-x-4">
+          <Heading>{username}</Heading>
+          {stampCount > 0 && (
+            <Text className="self-end">{stampCount} Stamps</Text>
+          )}
+        </div>
+        <Text className="text-sm">{biography}</Text>
+      </div>
       <Grid>
-        {user.listedStamps.map((stamp) => (
-          <StampCard key={stamp.id} user={user} {...stamp} />
-        ))}
+        {stampCount > 0 ? (
+          listedStamps.map((stamp) => (
+            <StampCard
+              key={stamp.id}
+              user={{ username, usernameURL, image }}
+              {...stamp}
+            />
+          ))
+        ) : (
+          <Text>User has no stamps</Text>
+        )}
       </Grid>
     </Container>
   )
