f97fe9face36c9fb9975af5ae195a4ed0c2bf6cd
milank <milankornicer@gmail.com>
2024-10-29 10:56:51 -0400

    chore: profile route group and loading skeleton for user settings

---
+++ src/app/[user]/(profile)/loading.tsx
@@ -0,0 +1,13 @@
+import { Container, Subheading } from '@/components/ui'
+
+const SettingsPageLoading = () => {
+  return (
+    <Container>
+      <div className="grid max-w-3xl space-y-8">
+        <Subheading>Profile</Subheading>
+      </div>
+    </Container>
+  )
+}
+
+export default SettingsPageLoading
