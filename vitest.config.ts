8b37e94ef02c19da2e6f1b5cd2a4a0032cbbdd72
milank <milankornicer@gmail.com>
2024-12-03 16:27:18 -0500

    update vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,10 +2,10 @@
 
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths'
-import { defineConfig, type UserConfig } from 'vitest/config'
+import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
-  plugins: [tsconfigPaths(), react()] as UserConfig['plugins'],
+  plugins: [tsconfigPaths(), react()],
   test: {
     environment: 'jsdom',
     globals: true,
