3f613eb5404c9021e9515ee85a68d5280156bdd9
milank <milankornicer@gmail.com>
2024-12-09 11:38:54 -0500

    feat: game version switch buttons

---
+++ src/components/layout/VersionButtons.tsx
@@ -0,0 +1,27 @@
+'use client'
+
+import Image from 'next/image'
+
+import anno117 from '../../../public/anno117-icon.jpg'
+import anno1800 from '../../../public/anno1800-icon.webp'
+import { Button } from '../ui'
+
+export const VersionButtons = () => {
+  return (
+    <div className="29px py-1">
+      <Button disabled plain>
+        <Image alt="anno 117 logo icon" height={20} src={anno117} width={20} />
+        Soon
+      </Button>
+      <Button href="/" plain>
+        <Image
+          alt="anno 1800 logo icon"
+          height={20}
+          src={anno1800}
+          width={20}
+        />
+        1800
+      </Button>
+    </div>
+  )
+}
