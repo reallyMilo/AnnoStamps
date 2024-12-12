3ef2074752b725ed123bf7ed7922d532b3bc0c2f
milank <milankornicer@gmail.com>
2024-12-12 13:52:15 -0500

    chore: split auth layout from site layout

---
+++ src/app/(auth)/not-found.tsx
@@ -0,0 +1,10 @@
+import { Container, Heading } from '@/components/ui'
+
+const Custom404 = () => {
+  return (
+    <Container className="flex flex-col justify-center gap-4">
+      <Heading className="text-center text-4xl">404 - Page not found</Heading>
+    </Container>
+  )
+}
+export default Custom404
