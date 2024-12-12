55d8e5ab3bf1a7024c14aa61fd2b27dacf9a1714
Milan K <57148574+mkcy3@users.noreply.github.com>
2024-12-12 13:12:00 -0500

    chore: use path mapping for image imports instead of absolute (#790)

--- src/app/how-to/page.tsx
+++ src/app/how-to/page.tsx
@@ -1,14 +1,13 @@
 import type { Metadata } from 'next'
 
+import stampButton from '@/../public/stamp-button.jpg'
+import stampPage from '@/../public/stamp-download.png'
+import stampPath from '@/../public/stamp-folder-path.jpg'
+import stampHighlight from '@/../public/stamp-highlight.jpg'
+import stampFolder from '@/../public/stamp-name.jpg'
 import Image from 'next/image'
 
 import { Container, Heading, Subheading, Text, TextLink } from '@/components/ui'
-
-import stampButton from '../../../public/stamp-button.jpg'
-import stampPage from '../../../public/stamp-download.png'
-import stampPath from '../../../public/stamp-folder-path.jpg'
-import stampHighlight from '../../../public/stamp-highlight.jpg'
-import stampFolder from '../../../public/stamp-name.jpg'
 
 export const metadata: Metadata = {
   description: `Stamps allow for creating blueprints of a layout that can be reused.
