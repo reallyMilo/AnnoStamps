e2425d57ce99d993e1945c46c2c8c25ae64967e7
milank <milankornicer@gmail.com>
2025-02-07 11:48:52 -0500

    report issues and fix types in handler

--- aws/lambdas/googleAnalyticsToDb/index.ts
+++ aws/lambdas/googleAnalyticsToDb/index.ts
@@ -1,21 +1,20 @@
 import { BetaAnalyticsDataClient } from '@google-analytics/data'
 import { createClient } from '@supabase/supabase-js'
-
+import { Handler } from 'aws-lambda'
 const analyticsDataClient = new BetaAnalyticsDataClient()
 
-export const handler = async () => {
+export const handler: Handler = async () => {
+  const supabaseURL = process.env.SUPABASE_DB_URL
+  const supabaseServiceKey = process.env.SUPABASE_SERVICE_KEY
+
+  if (!supabaseURL || !supabaseServiceKey) {
+    throw new Error('Missing supabase env')
+  }
   const [response] = await analyticsDataClient.runReport({
-    property: `properties/365198329`,
-    dimensions: [{ name: 'pagePath' }],
-    metrics: [
-      {
-        name: 'eventCount',
-      },
-    ],
     dateRanges: [
       {
-        startDate: 'yesterday',
         endDate: 'yesterday',
+        startDate: 'yesterday',
       },
     ],
     dimensionFilter: {
@@ -26,21 +25,42 @@ export const handler = async () => {
         },
       },
     },
+    dimensions: [{ name: 'pagePath' }],
+    metrics: [
+      {
+        name: 'eventCount',
+      },
+    ],
+    property: `properties/365198329`,
   })
 
-  console.log(`${response.rowCount} rows received`)
-  const supabase = createClient(
-    process.env.SUPA_DB,
-    process.env.SUPA_SERVICE_KEY,
-  )
+  if (!response.rows) {
+    throw new Error('Response rows are null')
+  }
+  const supabase = createClient(supabaseURL, supabaseServiceKey)
 
-  const collection = response.rows.map((row) => ({
-    stampId: row.dimensionValues[0].value.split('/')[2],
-    increment: Number(row.metricValues[0].value),
-  }))
+  const collection = response.rows.map((row) => {
+    if (
+      !row.dimensionValues ||
+      !row.dimensionValues[0] ||
+      !row.dimensionValues[0].value ||
+      !row.metricValues
+    ) {
+      console.error('Missing download count or stampId on row', row)
+      return {
+        increment: 0,
+        stampId: '',
+      }
+    }
+
+    return {
+      increment: Number(row.metricValues[0].value),
+      stampId: row.dimensionValues[0].value.split('/')[2],
+    }
+  })
 
   const { error } = await supabase.rpc('loopdownloads', {
     jsonb_collection: collection,
   })
-  console.log(error)
+  console.error(error)
 }
