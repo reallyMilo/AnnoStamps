7433938548610f2bde652b3a7c933e7416ff3c06
Milan K <57148574+mkcy3@users.noreply.github.com>
2025-02-12 10:26:43 -0500

    fix: cors error incapable of uploading images (#880)

--- aws/s3.tf
+++ aws/s3.tf
@@ -14,7 +14,7 @@ resource "aws_s3_bucket_cors_configuration" "annostamps_cors" {
   bucket = aws_s3_bucket.annostamps-bucket.id
   cors_rule {
     allowed_headers = ["*"]
-    allowed_methods = ["GET", "POST"]
+    allowed_methods = ["GET","PUT", "POST"]
     allowed_origins = ["https://annostamps.vercel.app",
       "https://annostamps.vercel.app/",
       "https://annostamps.com",
