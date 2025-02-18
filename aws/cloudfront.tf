37750284e1340461df64a52386a0db12c897e438
milank <milankornicer@gmail.com>
2025-02-18 10:26:04 -0500

    cloudfront

---
+++ aws/cloudfront.tf
@@ -0,0 +1,90 @@
+
+import {
+  to = aws_cloudfront_distribution.s3_distribution
+  id = "E2FHHL8ELSFRK9"
+}
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name              = aws_s3_bucket.tfbucket.bucket_domain_name
+    origin_access_control_id = aws_cloudfront_origin_access_control.default.id
+    origin_id                = "annostamps.s3.eu-central-1.amazonaws.com"
+  }
+
+  http_version        = "http2and3"
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "Some comment"
+  default_root_object = "anno-stamps-logo.png"
+
+
+  #aliases = ["annostamps.com"]
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "annostamps.s3.eu-central-1.amazonaws.com"
+
+
+    smooth_streaming = false
+    compress         = true
+
+    cache_policy_id            = data.aws_cloudfront_cache_policy.MangedCachingOptimized.id
+    response_headers_policy_id = aws_cloudfront_response_headers_policy.this.id
+    viewer_protocol_policy     = "redirect-to-https"
+
+  }
+  #PriceClass_100 = North America and Europe only
+  price_class = "PriceClass_100"
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
+import {
+    to = aws_cloudfront_origin_access_control.default
+    id = "E19JAL7W4CKWK9"
+}
+
+resource "aws_cloudfront_origin_access_control" "default" {
+  name                              = "tf-test-OAC"
+  description                       = "OAC TEST"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
+  origin_access_control_origin_type = "s3"
+}
+
+import {
+  to = aws_cloudfront_response_headers_policy.policy
+  id = "696c9467-8dc6-416f-8711-5944547445e0"
+}
+resource "aws_cloudfront_response_headers_policy" "policy" {
+  name = "allowAnnostamps-tf-test"
+
+  cors_config {
+    access_control_allow_credentials = false
+    origin_override                  = true
+    access_control_allow_headers {
+      items = ["annostamps.com"]
+    }
+
+    access_control_allow_methods {
+      items = ["GET"]
+    }
+    access_control_allow_origins {
+      items = ["https://annostamps.com"]
+    }
+
+  }
+}
+
+
+data "aws_cloudfront_cache_policy" "MangedCachingOptimized" {
+  id = "658327ea-f89d-4fab-a63d-7e88639e58f6"
+}
+
