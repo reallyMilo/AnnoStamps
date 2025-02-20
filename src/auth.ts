82bf639b8c961047aaf0f9fd81242315b975c1d0
milank <milankornicer@gmail.com>
2025-02-20 14:33:18 -0500

    feat: add avatarURL to user relation

--- src/auth.ts
+++ src/auth.ts
@@ -59,6 +59,7 @@ const config = {
         ...session,
         user: {
           ...session.user,
+          avatarURL: user.avatarURL,
           biography: user.biography,
           username: user.username,
           usernameURL: user.usernameURL,
