e074ec880bfb88178e3954b2931e2c171f021d46
milank <milankornicer@gmail.com>
2025-03-20 13:31:08 -0400

    fix hardcoded navigation

--- src/components/layout/VersionButtons.tsx
+++ src/components/layout/VersionButtons.tsx
@@ -3,17 +3,22 @@
 import anno117 from '@/../public/anno117-icon.jpg'
 import anno1800 from '@/../public/anno1800-icon.webp'
 import Image from 'next/image'
+import { usePathname } from 'next/navigation'
 
 import { Button } from '@/components/ui'
 
 export const VersionButtons = () => {
+  const pathname = usePathname()
   return (
     <div className="29px py-1">
-      <Button href="http://localhost:3000" plain>
+      <Button href={pathname} plain>
         <Image alt="anno 117 logo icon" height={20} src={anno117} width={20} />
         117
       </Button>
-      <Button href="http://1800.localhost:3000" plain>
+      <Button
+        href={`1800.${process.env.VERCEL_PROJECT_PRODUCTION_URL + pathname}`}
+        plain
+      >
         <Image
           alt="anno 1800 logo icon"
           height={20}
