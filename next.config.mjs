aa2a64fb75204ec8eb8117be8b784438ed179997
milank <milankornicer@gmail.com>
2025-03-20 14:54:36 -0400

    remove eslitn and ts ignore on build

--- next.config.mjs
+++ next.config.mjs
@@ -9,11 +9,6 @@ const withBundleAnalyzer = NextBundleAnalyzer({
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
   images: {
     remotePatterns: [
       {
@@ -43,13 +38,6 @@ const nextConfig = {
     ],
   },
   reactStrictMode: true,
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
 }
 
 export default withBundleAnalyzer(
