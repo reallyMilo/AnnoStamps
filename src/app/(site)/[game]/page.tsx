c65eb86f40a0028b81e75a9637d22ac34f1bd717
milank <milankornicer@gmail.com>
2025-03-24 09:29:52 -0400

    move back

--- src/app/(site)/[game]/page.tsx
+++
@@ -1,7 +0,0 @@
-import { Text } from '@/components/ui'
-
-const PreviousVersionHomePage = ({ params }: { params: { game: string } }) => {
-  return <Text>Home page for: {params.game}</Text>
-}
-
-export default PreviousVersionHomePage
