c96b5bd978aa3f0ee6d97ef3e34604185252c77e
milank <milankornicer@gmail.com>
2025-04-03 10:15:22 -0400

    lowercase s

--- cypress/e2e/version-switching.cy.ts
+++ cypress/e2e/version-switching.cy.ts
@@ -42,7 +42,7 @@ describe('Switch between game versions via navigation links and see correspondin
     cy.setSessionCookie()
     cy.visit('/stamp/create')
 
-    cy.findByText('Upload 117 Stamp').should('exist')
+    cy.findByText('Upload 117 stamp').should('exist')
 
     cy.getBySel('version-1800-link').click()
 
