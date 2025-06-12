c44a9e8184edd7d593f6eebcb7126931d51a0e7c
mkcy3 <milankornicer@gmail.com>
2025-06-12 12:45:06 -0400

    feat: add version based navigation links to user menu

--- vitest.setup.ts
+++ vitest.setup.ts
@@ -1,22 +1,19 @@
 import '@testing-library/jest-dom'
-
 import { beforeAll, vi } from 'vitest'
 const ResizeObserverMock = vi.fn(() => ({
+  disconnect: vi.fn(),
   observe: vi.fn(),
   unobserve: vi.fn(),
-  disconnect: vi.fn(),
 }))
 
 vi.stubGlobal('ResizeObserver', ResizeObserverMock)
 beforeAll(() => {
   vi.mock('next/navigation', () => ({
+    usePathname: vi.fn(() => ''),
     useRouter() {
       return {
         prefetch: () => null,
       }
-    },
-    usePathname() {
-      return {}
     },
     useSearchParams() {
       return {}
