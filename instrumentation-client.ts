e281e1b09eb4131010bcf315ba20b68c018fe11c
mkcy3 <milankornicer@gmail.com>
2025-06-16 12:49:22 -0400

    conditionally call sentry init on production

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -4,28 +4,31 @@
 
 import * as Sentry from '@sentry/nextjs'
 
-Sentry.init({
-  dsn: 'https://a61bb8933f45b2800ce5ffa1c28d9305@o4506508177244160.ingest.sentry.io/4506508178817024',
+if (process.env.NODE_ENV === 'production') {
+  Sentry.init({
+    allowUrls: ['https://annostamps.com'],
 
-  // Adjust this value in production, or use tracesSampler for greater control
-  tracesSampleRate: 1,
+    // Setting this option to true will print useful information to the console while you're setting up Sentry.
+    debug: false,
 
-  // Setting this option to true will print useful information to the console while you're setting up Sentry.
-  debug: false,
+    dsn: 'https://a61bb8933f45b2800ce5ffa1c28d9305@o4506508177244160.ingest.sentry.io/4506508178817024',
+    enabled: process.env.NODE_ENV === 'production',
+    // You can remove this option if you're not planning to use the Sentry Session Replay feature:
+    integrations: [
+      Sentry.replayIntegration({
+        unblock: ['.sentry-unblock, [data-sentry-unblock]'],
+        unmask: ['.sentry-unmask, [data-sentry-unmask]'],
+      }),
+    ],
 
-  replaysOnErrorSampleRate: 1.0,
+    replaysOnErrorSampleRate: 1.0,
 
-  // This sets the sample rate to be 10%. You may want this to be 100% while
-  // in development and sample at a lower rate in production
-  replaysSessionSampleRate: 0.1,
-
-  enabled: process.env.NODE_ENV === 'production',
-  allowUrls: ['https://annostamps.com'],
-  // You can remove this option if you're not planning to use the Sentry Session Replay feature:
-  integrations: [
-    Sentry.replayIntegration({
-      unblock: ['.sentry-unblock, [data-sentry-unblock]'],
-      unmask: ['.sentry-unmask, [data-sentry-unmask]'],
-    }),
-  ],
-})
+    // This sets the sample rate to be 10%. You may want this to be 100% while
+    // in development and sample at a lower rate in production
+    replaysSessionSampleRate: 0.1,
+    sendDefaultPii: true,
+    // Adjust this value in production, or use tracesSampler for greater control
+    tracesSampleRate: 1,
+  })
+}
+export const onRouterTransitionStart = Sentry.captureRouterTransitionStart
