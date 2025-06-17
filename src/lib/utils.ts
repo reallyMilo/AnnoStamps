6b40e6193ddc4ffc1fadd3ed0383443424bc463b
Milan K <57148574+mkcy3@users.noreply.github.com>
2025-06-17 14:40:59 -0400

    refactor: remove server action type (#1036)

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,6 +4,3 @@ import { twMerge } from 'tailwind-merge'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
-// https://github.com/vercel/next.js/issues/54156
-// https://github.com/vercel/next.js/pull/67211 fixed in next 15
-export type ServerAction<T, U> = ((data: T) => Promise<U>) & Function //eslint-disable-line
