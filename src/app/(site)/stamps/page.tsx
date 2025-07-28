f9aad8ae340c902ed2e4cf2781fb6318c4a08e13
mkcy3 <milankornicer@gmail.com>
2025-07-28 13:36:52 -0400

    refactor stamps page

--- src/app/(site)/stamps/page.tsx
+++ src/app/(site)/stamps/page.tsx
@@ -1,15 +1,47 @@
 import type { Metadata } from 'next'
 
+import { unstable_cache } from 'next/cache'
+import 'server-only'
+
+import { StampCard } from '@/components/StampCard'
+import { type QueryParams, queryParamsSchema } from '@/lib/constants'
+import prisma from '@/lib/prisma/singleton'
 import { StampGallery } from '@/view/StampGallery'
 
 export const metadata: Metadata = {
   title: `117 Stamps | AnnoStamps`,
 }
 
+const getFilteredStamps = unstable_cache(
+  async (query: QueryParams) => prisma.stamp.filterFindManyWithCount(query),
+  ['filterStamps'],
+  {
+    revalidate: 900,
+    tags: ['filterStamps'],
+  },
+)
+
 const StampsPage = async (props: {
   searchParams: Promise<{ [key: string]: string | string[] | undefined }>
 }) => {
   const searchParams = await props.searchParams
-  return <StampGallery searchParams={{ ...searchParams, game: '117' }} />
+
+  const parseResult = queryParamsSchema.safeParse(searchParams)
+  const filteredStamps = await getFilteredStamps(
+    parseResult.success ? parseResult.data : {},
+  )
+  return (
+    <StampGallery
+      paginatedStamps={{
+        ...filteredStamps,
+        stampsLength: filteredStamps.stamps.length,
+      }}
+      searchParams={{ ...searchParams, game: '117' }}
+    >
+      {filteredStamps.stamps.map((stamp) => (
+        <StampCard key={stamp.id} {...stamp} />
+      ))}
+    </StampGallery>
+  )
 }
 export default StampsPage
