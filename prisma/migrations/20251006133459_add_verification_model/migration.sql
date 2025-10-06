c8e1f007714e62ef9c379fd19793dd9824d56694
Milan K <57148574+mkcy3@users.noreply.github.com>
2025-10-06 09:45:19 -0400

    add missing fields and models (#1182)

---
+++ prisma/migrations/20251006133459_add_verification_model/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "public"."verification" (
+    "id" TEXT NOT NULL,
+    "identifier" TEXT NOT NULL,
+    "value" TEXT NOT NULL,
+    "expiresAt" TIMESTAMP(3) NOT NULL,
+    "createdAt" TIMESTAMP(3),
+    "updatedAt" TIMESTAMP(3),
+
+    CONSTRAINT "verification_pkey" PRIMARY KEY ("id")
+);
